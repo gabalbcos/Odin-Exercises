34c45f96e65f892876aff6ee85059a498c3e1e6d
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-03-01 10:11:44 -0300

    Add 'Rock, Paper, Scissors' game final version

--- javascript.js
+++ javascript.js
@@ -28,35 +28,62 @@
 
 //Rock, Paper, Scissors Console Game
 
-
-  function getComputerChoice(){
-    //List of options
-    const options = ['rock', 'paper', 'scissors'];
+let score = [];
+//List of options
+const options = ['rock', 'paper', 'scissors'];
+ 
+function getComputerChoice(){
     let botChoice = options[Math.floor(Math.random()*3)];
-    console.log(botChoice);
+    console.log(`Computer choose: ${botChoice}`);
     return botChoice;
 }
 
-function playRound(){
-    let playerChoice = prompt(`Rock, Paper or Scissors?`).toLowerCase();
-    let b = getComputerChoice();
-        //Round Comparisson
-    if (playerChoice === b){
-        return "It's a draw!";}
-    else if ((playerChoice === 'paper' && b === 'rock')||
-    (playerChoice === 'rock' && b === 'scissors')||
-    (playerChoice === 'scissors' && b === 'paper')) {
-         score++;
-        return "You Win!";}
-    else ((playerChoice === 'paper' && b === 'scissors')||
-    (playerChoice === 'rock' && b === 'paper')||
-    (playerChoice === 'scissors' && b === 'rock')){
-        score--;
-        return "You Lose!";}
+function getPlayerChoice(){
+    let playerMove = prompt(`Rock, Paper or Scissors?`).toLowerCase();
+    //check for a valid input from the options list
+    while (options.indexOf(playerMove) === -1) {
+        playerMove = prompt('Select a valid option: Rock, Paper or Scissor.');}
+    console.log(`You choose: ${playerMove}`);
+    return playerMove;
 }
 
-function game(){
-    let score = 0;
-    
-    console.log(score);
+//play one round of the game
+function playRound(){
+    //get computer + player choice 
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    //Round Comparison
+    if (playerChoice === computerChoice){
+        return "It's a draw!";}
+        else if ((playerChoice === 'paper' && computerChoice === 'rock')||
+        (playerChoice === 'rock' && computerChoice === 'scissors')||
+        (playerChoice === 'scissors' && computerChoice === 'paper')) {
+            return "You won this round!";}
+        else {
+            return "You lose this round!";}
 }
+
+//play game x Times
+function game(gameTimes){
+      //reset score
+      score = [];
+    //set score
+        for (let i = 0; i < gameTimes; i++){
+        let result = playRound();
+        console.log(result);
+        if (result === "You won this round!"){
+            score.push(1);}
+        else if (result === "You lose this round!") {
+            score.push(-1);    
+        } else { score.push(0);}
+        console.log("Your score:" + score.reduce(function(a, b){return a + b}) + `/${gameTimes}`);
+       }
+    //end game result
+        let totalScore = score.reduce(function(a, b){return a + b});
+        if (totalScore > 0) {
+            console.log("You won the game!");
+        } else if (totalScore === 0){
+            console.log("No winners in the game!");
+        } else {console.log("You lost the game!");}
+}
+
